62fa53698d5d94e38fe9b602d6336acd172643ed
Anthony Hang <hang.anthony@gmail.com>
2022-09-02 14:49:01 -0400

    moving moles interval

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 let score = 0;
+
+let holes = document.getElementsByClassName("hole");
+setInterval(function () {
+	const randomHole = Math.floor(Math.random() * holes.length);
+	const movingMole = holes[randomHole];
+	movingMole.classList.toggle("mole");
+}, 500);
